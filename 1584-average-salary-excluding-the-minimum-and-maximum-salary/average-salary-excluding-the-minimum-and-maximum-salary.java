767f6243edd236df750920e7541ec0e40c272648
Suvarnavb <157972260+Suvarnavb@users.noreply.github.com>
2026-09-21 13:10:51 +0530

    Time: 2 ms (31.26%) | Memory: 45.7 MB (56.41%) - LeetSync

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -1,16 +1,28 @@
-import java.util.Arrays;
-
 class Solution {
     public double average(int[] salary) {
 
         Arrays.sort(salary);
 
         int sum = 0;
+        double avg;
 
-        for (int i = 1; i < salary.length - 1; i++) {
-            sum = sum + salary[i];
+        if(salary.length % 2 == 0) {
+
+            for(int i = 1; i < salary.length - 1; i++) {
+                sum = sum + salary[i];
+            }
+
+            avg = (double)sum / (salary.length - 2);
+
+        } else {
+
+            for(int i = 1; i < salary.length - 1; i++) {
+                sum = sum + salary[i];
+            }
+
+            avg = (double)sum / (salary.length - 2);
         }
 
-        return (double) sum / (salary.length - 2);
+        return avg;
     }
 }
